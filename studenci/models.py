d4abf6c31cf9aa5708659502fda4e1e080c7abc8
lo1cgsan <lo1cgsan@gmail.com>
2019-09-26 13:48:29 +0200

    studenci: model i migracja

--- studenci/models.py
+++ studenci/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-Class
+
+class Uczelnia(models.Model):
+    nazwa = models.CharField(verbose_name='uczelnia', max_length=30)
+
+
+class Miasto(models.Model):
+    nazwa = models.CharField(verbose_name='miasto', max_length=30)
+    kod = models.CharField(max_length=30, help_text="Wpisz kod pocztowy")
+
+
+class Student(models.Model):
+    imie = models.CharField(max_length=30)
+    nazwisko = models.CharField(max_length=30)
+    uczelnia = models.ForeignKey(Uczelnia, on_delete=models.SET_NULL, null=True)
+    miasto = models.ForeignKey(Miasto, on_delete=models.SET_NULL, null=True)
+    roks = models.CharField(max_length=3, blank=True, default=0)
+    dochod = models.DecimalField(max_digits=6, decimal_places=2, default=0)
