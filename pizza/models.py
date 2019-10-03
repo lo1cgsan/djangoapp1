c9dcb1624fe0cf7c72831efea4635dcc8d6159a4
lo1cgsan <lo1cgsan@gmail.com>
2019-10-03 08:35:02 +0200

    poprawki modeli Pizza i Skladnik

--- pizza/models.py
+++ pizza/models.py
@@ -12,18 +12,21 @@ class Pizza(models.Model):
     )
 
     nazwa = models.CharField(verbose_name='pizza', max_length=30)
-    opis = models.TextField(blank=True, help_text='Opis pizzy')
+    opis = models.TextField(blank=True, default='', help_text='Opis pizzy')
     rozmiar = models.CharField(max_length=1, choices=ROZMIARY, default=LARGE)
-    cena = models.DecimalField(max_digits=5, decimal_places=2)
+    cena = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     data = models.DateField('dodano', auto_now_add=True)
 
+    def __str__(self):
+        return self.nazwa
 
 class Skladnik(models.Model):
-    pizza = models.ForeignKey(Pizza,
-                              on_delete=models.CASCADE,
-                              related_name='skladniki')
+    pizze = models.ManyToManyField(Pizza, related_name='skladniki')
     nazwa = models.CharField(verbose_name='składnik', max_length=30)
     jarski = models.BooleanField(
         verbose_name='jarski?',
         help_text='Zaznacz, jeżeli składnik jest odpowiedni dla wegetarian',
         default=False)
+
+    def __str__(self):
+        return self.nazwa
