436e99687542a56e67fd6249d49e708dc8f83490
lo1cgsan <lo1cgsan@gmail.com>
2019-10-17 07:53:20 +0200

    widok dodawania i wyświetlania miast - cz.1

--- studenci/views.py
+++ studenci/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from studenci.models import Miasto
 
 def index(request):
     return HttpResponse("<h1>Witaj wsród sudentów!</h1>")
     # return render(request,'pizza/index.html')
+
+
+def miasta(request):
+    """Widok wyświetlający miasta i formularz ich dodawania"""
+    miasta = Miasto.objects.all()
+    kontekst = {'miasta': miasta}
+    return render(request, 'studenci/miasta.html', kontekst)
