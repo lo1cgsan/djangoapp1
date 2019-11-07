28dac52b096d9db547856e84dfb4e05d712ed128
lo1cgsan <lo1cgsan@gmail.com>
2019-11-07 08:46:44 +0100

    dodawanie szablonu Bootstrap cz. 1

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'pizza'
 urlpatterns = [
 	path('', views.index, name='index'),
     path('komunikat/', views.komunikat, name='komunikat'),
--- studenci/views.py
+++ studenci/views.py
@@ -46,6 +46,7 @@ def uczelnie(request):
             u = Uczelnia(nazwa=form.cleaned_data['nazwa'])
             u.save()
             messages.success(request, "Poprawnie dodano dane!")
+            return redirect(reverse('studenci:uczelnie'))
         else:
             messages.error(request, "Niepoprawne dane!")
     else:
