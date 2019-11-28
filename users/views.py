c1ef68d90dff1bc5739a4325be79d8b4cffbc760
lo1cgsan <lo1cgsan@gmail.com>
2019-11-28 08:45:43 +0100

    początek obsługi logowania

--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
+from users.forms import UserLoginForm
 
 def index(request):
     pass
@@ -18,3 +21,17 @@ def rejestruj(request):
 
     kontekst = {'form': form}
     return render(request, 'users/rejestruj.html', kontekst)
+
+
+def loguj_user(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            login = form.cleaned_data['login']
+            haslo = form.cleaned_data['haslo']
+            pass
+    else:
+        form = UserLoginForm()
+
+    kontekst = {'form': form}
+    return render(request, 'users/loguj_user.html', kontekst)
