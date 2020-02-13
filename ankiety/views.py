e496d0caebe3488242545850528ba469eb6219cb
lo1cgsan <lo1cgsan@gmail.com>
2020-02-13 08:45:24 +0100

    początek widoku listy pytań

--- ankiety/views.py
+++ ankiety/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from ankiety.models import Pytanie, Odpowiedz
 
-# Create your views here.
+class ListaPytan(ListView):
+    model = Pytanie
+    template_name = 'ankiety/lista_pytan.html'
+    context_object_name = 'pytania'
+
+    def get_queryset(self):
+        return Pytanie.objects.order_by('-data_d')[:10]
